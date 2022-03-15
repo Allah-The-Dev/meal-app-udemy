e643c688018da77300bda28fa7a665fb3d786cb1
Neelesh kumar pandey <pandeyneelkumar@gmail.com>
2022-03-15 16:30:50 +0530

    first draft of meal app - code from udemy tutorial

---
+++ App.js
@@ -0,0 +1,34 @@
+import 'react-native-gesture-handler';
+import React, { useState } from 'react';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+import MealsNavigator from './navigation/MealsNavigator';
+import { enableScreens } from 'react-native-screens';
+
+// uses native screen blocks to make app perform much faster
+enableScreens();
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
+
+export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if(!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
+
+  return (
+    <MealsNavigator/>
+  );
+}
