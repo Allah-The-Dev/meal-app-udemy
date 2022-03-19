d86fe2786de2c3b05878d58151653fbb216d4e6f
Neelesh kumar pandey <pandeyneelkumar@gmail.com>
2022-03-19 11:14:19 +0530

    added redux to use initial state in the app

--- App.js
+++ App.js
@@ -1,24 +1,34 @@
-import 'react-native-gesture-handler';
-import React, { useState } from 'react';
-import * as Font from 'expo-font';
-import AppLoading from 'expo-app-loading';
-import MealsNavigator from './navigation/MealsNavigator';
-import { enableScreens } from 'react-native-screens';
+import "react-native-gesture-handler";
+import React, { useState } from "react";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+import { enableScreens } from "react-native-screens";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+
+import MealsNavigator from "./navigation/MealsNavigator";
+import mealsReducer from "./store/reducers/meals";
 
 // uses native screen blocks to make app perform much faster
 enableScreens();
 
+const rootReducer = combineReducers({
+  meals: mealsReducer,
+});
+
+const store = createStore(rootReducer);
+
 const fetchFonts = () => {
   return Font.loadAsync({
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
   });
 };
 
 export default function App() {
   const [fontLoaded, setFontLoaded] = useState(false);
 
-  if(!fontLoaded) {
+  if (!fontLoaded) {
     return (
       <AppLoading
         startAsync={fetchFonts}
@@ -29,6 +39,8 @@ export default function App() {
   }
 
   return (
-    <MealsNavigator/>
+    <Provider store={store}>
+      <MealsNavigator />
+    </Provider>
   );
 }
--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { ScrollView, View, Text, StyleSheet, Image } from "react-native";
-import { MEALS } from "../data/dummy-data";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { useSelector } from "react-redux";
+
 import HeaderButton from "../components/HeaderButton";
 import DefaultText from "../components/DefaultText";
 
@@ -15,8 +16,14 @@ const ListItem = (props) => {
 };
 
 const MealDetailsScreen = (props) => {
+  const availableMeals = useSelector((state) => state.meals.meals);
   const mealId = props.navigation.getParam("mealId");
-  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+  const selectedMeal = availableMeals.find((meal) => meal.id === mealId);
+
+  // useEffect(() => {
+  //   props.navigation.setParams({ mealTitle: selectedMeal.title });
+  // }, [selectedMeal]);
+
   return (
     <ScrollView>
       <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
@@ -38,10 +45,10 @@ const MealDetailsScreen = (props) => {
 };
 
 MealDetailsScreen.navigationOptions = (navigationData) => {
-  const mealId = navigationData.navigation.getParam("mealId");
-  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+  // const mealId = navigationData.navigation.getParam("mealId");
+  const mealTitle = navigationData.navigation.getParam('mealTitle');
   return {
-    headerTitle: selectedMeal.title,
+    headerTitle: mealTitle,
     headerRight: () => (
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
         <Item
@@ -76,8 +83,8 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
     borderColor: "#ccc",
     borderWidth: 1,
-    padding: 10
-  }
+    padding: 10,
+  },
 });
 
 export default MealDetailsScreen;
